ffd123044fcc92a21bd82690e0609c419aa70aaa
yunqi <yunqi@foxmail.com>
2021-04-01 14:08:53 +0800

    tow_sum test

--- subject/Array/two_sum/two_sum_test.go
+++ subject/Array/two_sum/two_sum_test.go
@@ -5,10 +5,15 @@ import (
 	"testing"
 )
 
+var numsArr = []int{1, 2, 3, 7, 9, 10, 5}
+
 func TestTwoSumMap(t *testing.T) {
-	numsArr := []int{1, 2, 3, 7, 9, 10, 5}
 	// map
 	fmt.Println(twoSumMap(numsArr, 9)) //O(n)
+
+}
+
+func TestTwoSum(t *testing.T) {
 	// 暴力枚举
 	fmt.Println(twoSum(numsArr, 9)) //O(n²)
 }
